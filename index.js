8a45ef8a235ea2cad368fb5cc9a187b8dce6d6de
Matheusfnf <matheusfortunatoaw@gmail.com>
2022-11-12 21:52:49 -0300

    joi and schema implemented

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 import express from "express";
 import cors from "cors";
 import { MongoClient } from "mongodb";
+import dotenv from "dotenv"
+import joi from "joi"
+import dayjs from "dayjs";
+
+const messageSchema = joi.object({
+  to: joi.string().required().min(3).max(30),
+  text: joi.string().required(),
+  type: joi.string().required(),
+});
+
+
+
+const participantSchema = joi.object({
+  name: joi.string().required().min(3).max(30),
+  
+});
+
 
 const app = express();
 
+dotenv.config();
 app.use(cors());
 app.use(express.json());
 
-const mongoClient = new MongoClient("mongodb://localhost:27017");
+const mongoClient = new MongoClient(process.env.MONGO_URI);
 let db;
 
 try {
@@ -16,6 +34,36 @@ try {
 } catch (err) {
   console.log(err);
 }
+
+app.get("/participants", async (req,res) => {
+  try{
+    const participants = await db.collection("participants").find().toArray();
+    res.send(participants)
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
+});
+
+app.post("/participants", async (req,res) => {
+  const body = req.body;
+
+  const validation = participantSchema.validate(body, {abortEarly: false});
+
+   if (validation.error) {
+     const errors = validation.error.details.map((detail) => detail.message);
+     res.send(errors);
+     return;
+   }
+
+  try {
+ await db.collection("participants").insert(body)
+ res.status(201).send("Participante entrou com sucesso!")
+  }
+  catch(err){
+    res.status(422).send(err)
+  }
+})
 
 app.get("/message", async (req, res) => {
   try {
@@ -29,6 +77,15 @@ app.get("/message", async (req, res) => {
 
 app.post("/message", async (req, res) => {
   const body = req.body;
+  const {User} = req.headers;
+
+  const validation = messageSchema.validate(body, {abortEarly: false});
+ 
+  if(validation.error){
+    const errors = validation.error.details.map(detail => detail.message)
+    res.send(errors)
+    return
+  }
 
   try {
     await db.collection("message").insert(body);
@@ -38,18 +95,15 @@ app.post("/message", async (req, res) => {
   }
 });
 
-// db.collection("message")
-//   .insert({
-//     to,
-//     text,
-//     type,
-//   })
-//   .then((response) => {
-//     console.log(response);
-//     res.status(201).send("mensagem enviada com sucesso!");
-//   })
-//   .catch((err) => {
-//     res.status(500).send(err);
-//   });
-
 app.listen(5000, () => console.log("server running on port 5000"));
+
+
+  /*
+  {
+    "from": "matheus",
+    "to": "julia",
+    "text": "oiiii",
+   "time": "20:42:12"
+  }
+*/
+
